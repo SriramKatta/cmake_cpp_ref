bcdc05428113feead83b87cecc976adfe94b8af0
Sriram Katta <sriram.katta@fau.de>
2025-03-16 12:36:09 +0100

    Add initial CMake configuration and basic project structure with a sample implementation

---
+++ header/header.hpp
@@ -0,0 +1,15 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#pragma once
+
+namespace test
+{
+
+    int superfunc()
+    {
+        return 42;
+    }
+}
+
+#endif
---
+++ src/main/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <header.hpp>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "hello world " <<  test::superfunc() << std::endl;
+    return 0;
+}
