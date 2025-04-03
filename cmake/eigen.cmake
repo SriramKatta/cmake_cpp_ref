87aebed9a51e514984b7f8722981466a7577b49c
Siram Katta <sriram.katta@fau.de>
2025-04-03 21:24:11 +0200

    Enhance eigen.cmake with comments for clarity and maintainability

--- cmake/eigen.cmake
+++ cmake/eigen.cmake
@@ -1,12 +1,17 @@
+# Use CPM to fetch and manage the Eigen library
 CPMAddPackage(
-  NAME Eigen
-  VERSION 3.2.8
-  URL https://gitlab.com/libeigen/eigen/-/archive/3.2.8/eigen-3.2.8.tar.gz
-  # Eigen's CMakelists are not intended for library use
-  DOWNLOAD_ONLY YES 
+  NAME Eigen  # Package name
+  VERSION 3.2.8  # Specify Eigen version
+  URL https://gitlab.com/libeigen/eigen/-/archive/3.2.8/eigen-3.2.8.tar.gz  # Download URL for Eigen
+
+  # Eigen does not provide a standard CMakeLists.txt for building as a library
+  DOWNLOAD_ONLY YES  # Only download Eigen, do not attempt to build it
 )
 
+# If Eigen was successfully added, define it as an imported interface library
 if(Eigen_ADDED)
-  add_library(Eigen INTERFACE IMPORTED)
+  add_library(Eigen INTERFACE IMPORTED)  # Create an imported interface target for Eigen
+
+  # Set Eigen's include directories so that projects using Eigen can find its headers
   target_include_directories(Eigen INTERFACE ${Eigen_SOURCE_DIR})
 endif()
