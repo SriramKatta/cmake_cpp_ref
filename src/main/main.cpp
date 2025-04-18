80974276242d9a3a3d07b7e7180796820b8d395f
Siram Katta <sriram.katta@fau.de>
2025-04-18 19:01:57 +0200

    Update CMake configuration and refactor main.cpp for improved readability and consistency

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,17 +1,17 @@
-#include <boost/mpi/environment.hpp>
+#include <fmt/format.h>
 #include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
 #include <iostream>
 namespace mpi = boost::mpi;
-namespace mt  = mpi::threading;
+namespace mt = mpi::threading;
 
-int main(int argc, char *argv[])
-{
-  mpi::environment env(argc, argv,mt::funneled);
+int main(int argc, char *argv[]) {
+  mpi::environment env(argc, argv, mt::funneled);
   if (env.thread_level() < mt::funneled) {
-     env.abort(-1);
+    env.abort(-1);
   }
   mpi::communicator world;
-  std::cout << "I am process " << world.rank() << " of " << world.size() << env.processor_name()
-            << "." << std::endl;
+  fmt::print("I am Process {} of {} on the host {}\n", world.rank(), world.size(),
+             env.processor_name());
   return 0;
 }
