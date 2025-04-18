ba3d1fa7bb36b9451ffebc42e71bba9a55f1075c
Siram Katta <sriram.katta@fau.de>
2025-04-18 18:33:31 +0200

    Add Boost and fmt libraries to CMake configuration and update main.cpp for MPI support

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,12 +1,14 @@
-set(BOOST_INCLUDE_LIBRARIES "program_options")
+set(GIT_SHALLOW TRUE)
+set(BOOST_ENABLE_CMAKE ON)
+set(BOOST_SKIP_INSTALL_RULES ON) # Set `OFF` for installation
+set(BUILD_SHARED_LIBS ON)
+set(BOOST_ENABLE_MPI ON)
+set(BOOST_INCLUDE_LIBRARIES "filesystem;serialization;mpi")
 
 CPMAddPackage(
   NAME Boost
   VERSION 1.86.0 # Versions less than 1.85.0 may need patches for installation targets.
   URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
-  URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
-  OPTIONS 
-    "BOOST_ENABLE_CMAKE ON" 
-    "BOOST_SKIP_INSTALL_RULES ON" # Set `OFF` for installation
-    "BUILD_SHARED_LIBS OFF"
+  URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57   
 )
+
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,76 +1,17 @@
-// Copyright Vladimir Prus 2002-2004.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt
-// or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#include <boost/program_options.hpp>
-
-using namespace boost;
-namespace po = boost::program_options;
-
-#include <algorithm>
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
 #include <iostream>
-#include <iterator>
-using namespace std;
+namespace mpi = boost::mpi;
+namespace mt  = mpi::threading;
 
-
-// A helper function to simplify the main part.
-template <class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-  copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
-  return os;
-}
-
-int main(int ac, char *av[]) {
-  try {
-    int opt;
-    int portnum;
-    po::options_description desc("Allowed options");
-    desc.add_options()("help,h", "produce help message")(
-      "optimization", po::value<int>(&opt)->default_value(10),
-      "optimization level")("verbose,v", po::value<int>()->implicit_value(1),
-                            "enable verbosity (optionally specify level)")(
-      "listen,l",
-      po::value<int>(&portnum)->implicit_value(1001)->default_value(0, "no"),
-      "listen on a port.")("include-path,I", po::value<vector<string>>(),
-                           "include path")(
-      "input-file", po::value<vector<string>>(), "input file");
-
-    po::positional_options_description p;
-    p.add("input-file", -1);
-
-    po::variables_map vm;
-    po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(),
-              vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-      cout << "Usage: options_description [options]\n";
-      cout << desc;
-      return 0;
-    }
-
-    if (vm.count("include-path")) {
-      cout << "Include paths are: " << vm["include-path"].as<vector<string>>()
-           << "\n";
-    }
-
-    if (vm.count("input-file")) {
-      cout << "Input files are: " << vm["input-file"].as<vector<string>>()
-           << "\n";
-    }
-
-    if (vm.count("verbose")) {
-      cout << "Verbosity enabled.  Level is " << vm["verbose"].as<int>()
-           << "\n";
-    }
-
-    cout << "Optimization level is " << opt << "\n";
-
-    cout << "Listen port is " << portnum << "\n";
-  } catch (std::exception &e) {
-    cout << e.what() << "\n";
-    return 1;
+int main(int argc, char *argv[])
+{
+  mpi::environment env(argc, argv,mt::funneled);
+  if (env.thread_level() < mt::funneled) {
+     env.abort(-1);
   }
+  mpi::communicator world;
+  std::cout << "I am process " << world.rank() << " of " << world.size() << env.processor_name()
+            << "." << std::endl;
   return 0;
 }
